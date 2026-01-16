0ea2f69588869067a45a28eb3cff509827507461
Radio Warning FM <warningfm@gmail.com>
2026-01-16 14:06:51 +0700

    Update icast.js

--- icast/js/icast.js
+++ icast/js/icast.js
@@ -14,7 +14,7 @@
 			enable_cors: false,
 			cors: "https://zet.pluginsandthemes.ro",			
 			artwork: true,
-            logo: "https://warningfm.github.io/v3/img/eDMLJdf.png",
+            logo: "https://warningfm.github.io/icast/img/logo.jpg",
 			servertitle: "My Radio Title", //For Shoutcast v1 server
             show_listeners: false,    
             src: "",
