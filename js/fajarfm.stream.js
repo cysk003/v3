c86d9c663b20b12b87948c547d7f072e98877347
Radio Warning FM <warningfm@gmail.com>
2026-01-31 12:15:41 +0700

    Update fajarfm.stream.js

--- js/fajarfm.stream.js
+++ js/fajarfm.stream.js
@@ -10,7 +10,7 @@
         port: 27215,
         protocol: 'http',
         version: 1,
-        directStreamURL: 'https://uk25freenew.listen2myradio.com/live.mp3?typeportmount=s1_27215_stream_535175960',
+        directStreamURL: 'https://uk25freenew.listen2myradio.com/live.mp3?typeportmount=s1_27215_stream_535175960|Referer=https://fajarfm.radio12345.com/',
         defaultArtwork: ['https://warningfm.github.io/v3/images/1d205655ef29e14a8255c89fe2383a41.jpg'],
         statusBar: false,
         startTemplate: 'maximized',
