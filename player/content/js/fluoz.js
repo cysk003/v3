5a79fb0ccfe62cd8f45bac4b0d327bf8448b5f39
Radio Warning FM <warningfm@gmail.com>
2026-03-29 22:32:10 +0700

    Update fluoz.js

--- player/content/js/fluoz.js
+++ player/content/js/fluoz.js
@@ -714,7 +714,7 @@ var mrpjq = jQuery;
             useKeyboardNavigationForPlayback: !1,
             scrollOrientation: "vertical",
             scrollTheme: "minimal",
-            cors: "https://cors.mediastreaming.it/",
+            cors: "https://zet.pluginsandthemes.ro/",
             getPlayerArtwork: !0,
             getHistoryArtwork: !1,
             createHistoryList: !0,
